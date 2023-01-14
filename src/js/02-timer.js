5b2af446e0716fbc243d90b25e1b8b1dd02f0eb5
Bohdan Mazur <104269738+GlucoGen17@users.noreply.github.com>
2023-01-15 00:23:20 +0200

    task 2 use

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -111,5 +111,58 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from "notiflix";
 
-// <input type="text" id="datetime-picker" />
-// <button type="button" data-start>Start</button>
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(3600000)); // {days: 0, hours: 1, minutes: 0, seconds: 0}
+
+const refs = {
+  datetimePicker: document.querySelector("#datetime-picker"),
+  startBtn: document.querySelector("button[data-start]"),
+  days: document.querySelector("[data-days]"),
+  hours: document.querySelector("[data-hours]"),
+  minutes: document.querySelector("[data-minutes]"),
+  seconds: document.querySelector("[data-seconds]"),
+  
+};
+console.log(refs);
+
+let timerId = null;
+let endDate = null;
+refs.startBtn.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+    onClose(selectedDates) {
+        endDate = selectedDates[0];
+        if (endDate <= new Date()) {
+            refs.btnStart.disabled = true;
+            Notiflix.Notify.failure("Please choose a date in the future");
+        } else {
+        startBtn.disabled = false; 
+        }
+
+    console.log(selectedDates[0]);
+  },
+};
+
+
