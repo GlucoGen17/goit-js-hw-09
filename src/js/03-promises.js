1d98307c5c938a9accdb73b09eeef51360917585
Bohdan Mazur <104269738+GlucoGen17@users.noreply.github.com>
2023-01-17 13:05:57 +0200

    task 3 use

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,5 +1,7 @@
 import Notiflix from "notiflix";
 
+const form = document.querySelector(".form");
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   const promise = new Promise((resolve, reject) => {
@@ -15,4 +17,17 @@ function createPromise(position, delay) {
   return promise;
 }
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  RefsNumber = {
+    delay: Number(form.elements.delay.value),
+    step: Number(form.elements.step.value),
+    amount: Number(form.elements.amount.value),
+  };
+for (let i = 0; i < amount; i++) {
+  const element = array[i];
+  
+}
+}
+);
 
