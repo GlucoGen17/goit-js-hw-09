d34d7d3572bb57160383c9317882d9bd5f53f7c3
Bohdan Mazur <104269738+GlucoGen17@users.noreply.github.com>
2023-01-17 12:48:11 +0200

    task 2 finish

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,18 @@
+import Notiflix from "notiflix";
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+  return promise;
 }
+
+
